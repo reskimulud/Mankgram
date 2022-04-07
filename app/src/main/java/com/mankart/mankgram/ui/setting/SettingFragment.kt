a019ea351692c28de259472a94f5a76d61ccced7
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-07 21:13:02 +0700

    create bottom navigation

---
+++ app/src/main/java/com/mankart/mankgram/ui/setting/SettingFragment.kt
@@ -0,0 +1,39 @@
+package com.mankart.mankgram.ui.setting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.mankart.mankgram.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+    private val settingViewModel: SettingViewModel by activityViewModels()
+    private var _binding: FragmentSettingBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        settingViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
