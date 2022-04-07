f6895b9edbe8055fbdf54759dc3e3b93ca1cbf2c
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-07 12:08:09 +0700

    create splash screen activity

---
+++ app/src/main/java/com/mankart/mankgram/SplashScreenActivity.kt
@@ -0,0 +1,45 @@
+package com.mankart.mankgram
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val delayMillis: Long = 2000
+        setupView()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, delayMillis)
+    }
+
+    private fun setupView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.navigationBarColor = resources.getColor(R.color.black, null)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
+
+        @Suppress("DEPRECATION")
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        supportActionBar?.hide()
+
+    }
+}
