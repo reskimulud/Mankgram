de7d005c39c2f4848ef59087045db25cc84ea308
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-08 02:44:27 +0700

    create authentication page for login and register #5

--- app/src/main/java/com/mankart/mankgram/authentication/RegisterFragment.kt
+++ app/src/main/java/com/mankart/mankgram/authentication/RegisterFragment.kt
@@ -1,37 +1,35 @@
 package com.mankart.mankgram.authentication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.commit
 import com.mankart.mankgram.R
-import com.mankart.mankgram.databinding.FragmentLoginBinding
+import com.mankart.mankgram.databinding.FragmentRegisterBinding
 
-class LoginFragment : Fragment() {
-    private var _binding: FragmentLoginBinding? = null
+
+class RegisterFragment : Fragment() {
+    private var _binding: FragmentRegisterBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvRegister.setOnClickListener {
+        binding.tvLogin.setOnClickListener {
             activity?.supportFragmentManager?.commit {
-                replace(R.id.placeholder, RegisterFragment())
+                replace(R.id.placeholder, LoginFragment())
                 addToBackStack(null)
             }
         }
