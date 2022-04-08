bc55d2d2435b5ba78e476c122d542abfd8731db4
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-08 23:38:49 +0700

    update card list and add transition for description

--- app/src/main/java/com/mankart/mankgram/ListStoryAdapter.kt
+++ app/src/main/java/com/mankart/mankgram/ListStoryAdapter.kt
@@ -1,7 +1,12 @@
 package com.mankart.mankgram
 
+import android.animation.ObjectAnimator
+import android.content.res.Resources
+import android.transition.TransitionManager
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mankart.mankgram.databinding.ItemRowStoryBinding
@@ -12,6 +17,11 @@ class ListStoryAdapter : RecyclerView.Adapter<ListStoryAdapter.ListViewHolder>()
     inner class ListViewHolder(binding: ItemRowStoryBinding) : RecyclerView.ViewHolder(binding.root) {
         var image = binding.storyImage
         var name = binding.storyName
+        var description = binding.storyDescription
+        var detail = binding.tvDetail
+        var isExpanded = false
+
+        val getResources: Resources = binding.root.context.resources
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -22,12 +32,32 @@ class ListStoryAdapter : RecyclerView.Adapter<ListStoryAdapter.ListViewHolder>()
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val repo = listStory[position]
 
-        holder.name.text = repo.name
-        Glide.with(holder.itemView.context)
-            .load("https://via.placeholder.com/${holder.image.height}x${holder.image.width}")
-            .placeholder(R.drawable.placeholder_image)
-            .error(R.drawable.placeholder_image)
-            .into(holder.image)
+        holder.apply {
+            name.text = repo.name
+            Glide.with(itemView.context)
+                .load("https://via.placeholder.com/${image.height}x${image.width}")
+                .placeholder(R.drawable.placeholder_image)
+                .error(R.drawable.placeholder_image)
+                .into(image)
+
+            holder.detail.setOnClickListener {
+                TransitionManager.beginDelayedTransition(itemView as ViewGroup)
+                when (isExpanded) {
+                    true -> {
+                        description.visibility = View.GONE
+                        detail.text = getResources.getString(R.string.detail)
+                        detail.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_keyboard_arrow_down_24, 0, 0, 0)
+                        isExpanded = false
+                    }
+                    false -> {
+                        description.visibility = View.VISIBLE
+                        detail.text = getResources.getString(R.string.close)
+                        detail.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_keyboard_arrow_up_24, 0, 0, 0)
+                        isExpanded = true
+                    }
+                }
+            }
+        }
     }
 
     override fun getItemCount(): Int = listStory.size
