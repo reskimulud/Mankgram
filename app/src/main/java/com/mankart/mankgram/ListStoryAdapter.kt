099885737bd1e90366a19f2692f263e71237497b
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-08 22:00:37 +0700

    create story card

--- app/src/main/java/com/mankart/mankgram/ListStoryAdapter.kt
+++ app/src/main/java/com/mankart/mankgram/ListStoryAdapter.kt
@@ -3,13 +3,15 @@ package com.mankart.mankgram
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mankart.mankgram.databinding.ItemRowStoryBinding
 
 class ListStoryAdapter : RecyclerView.Adapter<ListStoryAdapter.ListViewHolder>() {
     private var listStory = ArrayList<StoryModel>()
 
-    class ListViewHolder(private val binding: ItemRowStoryBinding) : RecyclerView.ViewHolder(binding.root) {
-        var name = binding.tvName
+    inner class ListViewHolder(binding: ItemRowStoryBinding) : RecyclerView.ViewHolder(binding.root) {
+        var image = binding.storyImage
+        var name = binding.storyName
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -21,6 +23,11 @@ class ListStoryAdapter : RecyclerView.Adapter<ListStoryAdapter.ListViewHolder>()
         val repo = listStory[position]
 
         holder.name.text = repo.name
+        Glide.with(holder.itemView.context)
+            .load("https://via.placeholder.com/${holder.image.height}x${holder.image.width}")
+            .placeholder(R.drawable.placeholder_image)
+            .error(R.drawable.placeholder_image)
+            .into(holder.image)
     }
 
     override fun getItemCount(): Int = listStory.size
