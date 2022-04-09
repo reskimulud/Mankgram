c0a598d34995f48aefb52368bdb3777c37bfdca8
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-09 10:40:26 +0700

    create layout for settings #8

--- app/src/main/java/com/mankart/mankgram/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/mankart/mankgram/ui/setting/SettingFragment.kt
@@ -1,13 +1,17 @@
 package com.mankart.mankgram.ui.setting
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.mankart.mankgram.authentication.AuthenticationActivity
 import com.mankart.mankgram.databinding.FragmentSettingBinding
+import java.util.*
 
 class SettingFragment : Fragment() {
     private val settingViewModel: SettingViewModel by activityViewModels()
@@ -25,10 +29,20 @@ class SettingFragment : Fragment() {
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        settingViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+//        val textView: TextView = binding.textDashboard
+//        settingViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        binding.setLanguage.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
         }
+
+        binding.localName.text = Locale.getDefault().displayName
+        binding.btnLogout.setOnClickListener {
+            startActivity(Intent(activity, AuthenticationActivity::class.java))
+            activity?.finish()
+        }
+
         return root
     }
 
