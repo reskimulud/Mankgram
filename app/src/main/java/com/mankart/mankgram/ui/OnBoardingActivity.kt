e64d41c7570cd734c09d09ebe829480e236a5739
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-11 11:48:26 +0700

    finish animate on boarding

--- app/src/main/java/com/mankart/mankgram/ui/OnBoardingActivity.kt
+++ app/src/main/java/com/mankart/mankgram/ui/OnBoardingActivity.kt
@@ -1,16 +1,35 @@
 package com.mankart.mankgram.ui
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.activity.viewModels
 import com.mankart.mankgram.R
+import com.mankart.mankgram.databinding.ActivityOnBoardingBinding
+import com.mankart.mankgram.ui.authentication.AuthenticationActivity
+import com.mankart.mankgram.ui.mainmenu.setting.SettingViewModel
 
 class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnBoardingBinding
+    private lateinit var factory: ViewModelFactory
+    private val settingViewModel: SettingViewModel by viewModels { factory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        factory = ViewModelFactory.getInstance(this)
+
+        binding.button.setOnClickListener {
+            settingViewModel.saveIsFirstTime(false)
+            startActivity(Intent(this, AuthenticationActivity::class.java))
+            finish()
+        }
 
         setupView()
     }
@@ -27,4 +46,5 @@ class OnBoardingActivity : AppCompatActivity() {
         }
         supportActionBar?.hide()
     }
+
 }
